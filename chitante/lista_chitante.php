d5b4db993d73488ed2954c71d54b1548de8f7dfc
Bocskai Csaba <csabi98@yahoo.com>
2023-03-03 12:13:26 +0200

    v 2.10 improve lista chitante usability
    Joined numar + serie chitanta.
    Joined numar + serie factura.
    Inserted client name for easier document identification.
    Optimized css.

--- chitante/lista_chitante.php
+++ chitante/lista_chitante.php
@@ -9,10 +9,10 @@
             #wrapper {display: inline-block; margin:auto;}
 			table {border:1px solid; font-size:14px; margin-bottom:50px;}
 			table th, table td {border-right:1px solid;}
-			td {padding:10px 5px 0px; text-align:center;}
-			#nr_crt {width:20px;}
-			#cant, #serie, #aviz, #nr_crt {text-align:center;}
 			tr:hover {background-color:#ddd;}
+			td, th {padding:5px 10px; text-align:center;}
+			#nume_client {text-align:left;}
+			#imprima {border-right:none;}
 		</style>
 	</head>
 
@@ -36,19 +36,22 @@
 
 		<table>
 			
-			<th id="serie_chit">Serie</th>
-			<th id="nr_chit">Număr</th>
+			<th id="serie_chit">Serie și <br /> Număr</th>
             <th id="data_chit">Data</th>
             <th id="val_chit">Valoare</th>
-            <th id="factura">Serie Factură</th>
-            <th id="factura">Nr. Factură</th>
+            <th id="nume">Client</th>
+            <th id="factura">Serie și Nr. <br /> Factură</th>
             <th id="operatii" colspan="4">Operații</th>
             
 				<?php
 
 				error_reporting(E_ALL);
 
-				$sql = "SELECT * FROM chitante GROUP BY nr_chit ORDER BY nr_chit DESC";
+				$sql = "
+                    select a.serie_chit, a.nr_chit, a.data_chit, a.val_chit, a.serie_factura, a.numar_factura, b.numar_factura, b.nume
+                    from chitante a
+                    inner join facturi b on a.numar_factura = b.numar_factura
+                    group by nr_chit order by nr_chit desc;";
 				$result = $conn->query($sql);
 
 				if ($result->num_rows > 0) {
@@ -56,15 +59,14 @@
 						echo
                             '<tr>' . 
                                 
-                                '<td id="serie_chit">' . $row["serie_chit"] . '</td>' . "\r\n" .
-                                '<td id="nr_chit">' . $row["nr_chit"] . '</td>' . "\r\n" .
+                                '<td id="chitanta">' . $row["serie_chit"] . " " . $row["nr_chit"] . '</td>' . "\r\n" .
                                 '<td>' . $row["data_chit"] . '</td>' . "\r\n" .
                                 '<td id="val_chit">' . $row["val_chit"] . '</td>' . "\r\n" .
-                                '<td>' . $row["serie_factura"] . '</td>' . "\r\n" .
-                                '<td>' . $row["numar_factura"] . '</td>' . "\r\n" .
+                                '<td id="nume_client">' . $row["nume"] . '</td>' . "\r\n" .
+                                '<td>' . $row["serie_factura"] . " " . $row["numar_factura"] . '</td>' . "\r\n" .
                                 '<td><a href="editeaza_chitanta.php?nr_chit=' . $row[nr_chit] . '">Editeaza</a></td>' . "\r\n" .
                                 '<td><a href="sterge_chitanta.php?nr_chit=' . $row[nr_chit] . '" onClick="return confirmare();">Sterge</a></td>' . "\r\n" .
-                                '<td><a href="imprima_chitanta.php?numar_factura=' . $row[numar_factura] . '" target="_blank">Imprima</a></td>' . "\r\n" .
+                                '<td id="imprima"><a href="imprima_chitanta.php?numar_factura=' . $row[numar_factura] . '" target="_blank">Imprima</a></td>' . "\r\n" .
                             '</tr>' . "\r\n" .
                             '<script type=\'text/javascript\'>
                             function confirmare() {
